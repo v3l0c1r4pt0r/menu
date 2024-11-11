58adf559bfac9c62c099946c6211a5291933595f
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2024-11-11 11:29:20 +0100

    Search packages for menu plugins
    List all of them.

--- menu/__main__.py
+++ menu/__main__.py
@@ -1,5 +1,19 @@
+import pkgutil
+
+PLUGIN_PACKAGE_PREFIX = 'menu_plugin_'
+
+def find_plugins():
+  plugin_list = []
+  for p in pkgutil.iter_modules():
+    if p.name.startswith(PLUGIN_PACKAGE_PREFIX):
+      plugin_list.append(p.name)
+  return plugin_list
+
 def main():
-  print('Hello, World!')
+  print('Found following plugins:')
+  plugins = find_plugins()
+  for plugin in plugins:
+    print(f'\t{plugin}')
 
 if __name__ == '__main__':
   main()

93739f3f08586162f3f84d02c7115562a1f1be7e
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2024-11-11 15:35:38 +0100

    Make main module a class
    Avoid a lot of hacks by closing everything inside of a class.

--- menu/__main__.py
+++ menu/__main__.py
@@ -11,44 +11,46 @@ PLUGIN_PACKAGE_PREFIX = 'menu_plugin_'
 log = Logger('menu').main_logger
 log.debug(f'Logger instantiated')
 
-def find_plugins():
-  plugin_list = []
-  for p in pkgutil.iter_modules():
-    if p.name.startswith(PLUGIN_PACKAGE_PREFIX):
-      plugin_list.append(p.name)
-  return plugin_list
+class Menu:
 
-def register_plugin(plugin_name):
-  try:
-    plugin = importlib.import_module(f'{plugin_name}.plugin')
-    hook = Hook()
-    log.debug(f'Hook created: {hook}')
-    plugin.register(hook)
-  except ModuleNotFoundError:
-    log.error(f'Plugin {plugin_name} is broken Skipping')
+  def __init__(self):
+    self.create_ui()
+    self.plugins = self.find_plugins()
 
-def create_ui():
-  parser = argparse.ArgumentParser(description='Menu', prog='m')
-  subparsers = parser.add_subparsers(required=True)
+  def main(self, argv):
+    for plugin in self.plugins:
+      log.debug(f'Processing plugin: {plugin}')
+      self.register_plugin(plugin)
 
-  return parser, subparsers
+    return self.execute(argv[1:], self.parser, self.subparsers)
 
-def execute(argv, parser, subparsers):
-  if len(argv) == 0:
-    parser.print_usage()
-    return 255
-  args = parser.parse_args(argv)
-  return args.func(args)
+  def find_plugins(self):
+    plugin_list = []
+    for p in pkgutil.iter_modules():
+      if p.name.startswith(PLUGIN_PACKAGE_PREFIX):
+        plugin_list.append(p.name)
+    return plugin_list
 
-def main(argv):
-  parser, subparsers = create_ui()
+  def register_plugin(self, plugin_name):
+    try:
+      plugin = importlib.import_module(f'{plugin_name}.plugin')
+      hook = Hook()
+      log.debug(f'Hook created: {hook}')
+      plugin.register(hook)
+    except ModuleNotFoundError:
+      log.error(f'Plugin {plugin_name} is broken Skipping')
 
-  plugins = find_plugins()
-  for plugin in plugins:
-    log.debug(f'Processing plugin: {plugin}')
-    register_plugin(plugin)
+  def create_ui(self):
+    self.parser = argparse.ArgumentParser(description='Menu', prog='m')
+    self.subparsers = self.parser.add_subparsers(required=True)
 
-  return execute(argv[1:], parser, subparsers)
+  def execute(self, argv, parser, subparsers):
+    if len(argv) == 0:
+      parser.print_usage()
+      return 255
+    args = parser.parse_args(argv)
+    return args.func(args)
+
 
 if __name__ == '__main__':
-  sys.exit(main(sys.argv))
+  sys.exit(Menu().main(sys.argv))

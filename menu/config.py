c58daba4203d2bddd418269f6be5401ce11d5648
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2025-11-16 14:56:51 +0100

    Add config manipulation API

--- menu/config.py
+++ menu/config.py
@@ -8,8 +8,19 @@ class Config:
   def __init__(self, root, name):
     self.location = root
     self.name = name
+    self.obj = None
 
-  """Get config object for plugin"""
-  def get(self):
-    log.debug(f'Getting config for {self.name}')
+  def load(self):
+    log.debug(f'Reading config from {self.location / self.name}')
     return None
+
+  """Get property from config"""
+  def get(self, prop):
+    log.debug(f'Getting property {prop} for {self.name}')
+    return None
+
+  def set(self, prop, value):
+    log.debug(f'Attempted change to {prop} = {value}')
+
+  def save(self):
+    log.debug(f'Saving config for {self.name}')

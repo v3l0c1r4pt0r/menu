318d2f9012014179509e3f1793a71c4a6c1a68fd
v3l0c1r4pt0r <v3l0c1r4pt0r@gmail.com>
2025-11-16 19:04:37 +0100

    Implement reading values from config
    Including all first run logic, like config file creation.

--- menu/config.py
+++ menu/config.py
@@ -1,3 +1,5 @@
+import json
+
 from menu.logger import Logger
 
 log = Logger.get(__name__)
@@ -10,14 +12,30 @@ class Config:
     self.name = name
     self.obj = None
 
+  def create(self, file):
+    if not file.parent.exists():
+      file.parent.mkdir(parents=True, exist_ok=True)
+    # defaults:
+    obj = {
+      'property': 'value',
+    }
+    with open(file, 'w') as fp:
+      json.dump(obj, fp)
+
   def load(self):
-    log.debug(f'Reading config from {self.location / self.name}')
-    return None
+    file = self.location / f'{self.name}.json'
+    log.debug(f'Reading config from {file}')
+    if not file.exists():
+      self.create(file)
+    with open(file, 'r') as fp:
+      self.obj = json.load(fp)
 
   """Get property from config"""
   def get(self, prop):
     log.debug(f'Getting property {prop} for {self.name}')
-    return None
+    if self.obj is None:
+      self.load()
+    return self.obj[prop]
 
   def set(self, prop, value):
     log.debug(f'Attempted change to {prop} = {value}')
